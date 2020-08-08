ead226e39f4a21428afe703797d4d67c4ce0c52f
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-08-08 15:43:11 -0500

    initial gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,15 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+        ],
+      },
+    },
+  ],
 }
