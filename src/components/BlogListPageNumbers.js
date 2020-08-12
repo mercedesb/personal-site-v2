6e3a59a8bdba7089665f8593c2087bd27d863301
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-08-12 09:43:24 -0500

    handle page number urls correctly on category pages

--- src/components/BlogListPageNumbers.js
+++ src/components/BlogListPageNumbers.js
@@ -8,12 +8,50 @@ export function BlogListPageNumbers({
   pageCount,
   path,
 }) {
+  const getPageNumber = index => {
+    let indexOffset = 2
+    let pageNumber = index + 1
+
+    if (currentPage > indexOffset) {
+      pageNumber = currentPage + index - indexOffset
+    }
+
+    return pageNumber
+  }
+
+  const pageNumberIsActive = (pageNumber, urlPath) => {
+    let active = false
+    if (pageNumber === 1 && urlPath.match(/[^0-9\/]\/?$/)) {
+      active = true
+    } else if (
+      pageNumber > 1 &&
+      urlPath.match(new RegExp(`${pageNumber}\/?$`))
+    ) {
+      active = true
+    }
+    return active
+  }
+
+  const getBasePath = () => {
+    const regexToRemoveLeadingTrailingSlashAndPageNumber = /^.|(\/?\d?\/?$)(?!.*\d)/g
+    return decodeURI(path).replace(
+      regexToRemoveLeadingTrailingSlashAndPageNumber,
+      ""
+    )
+  }
+
+  const basePath = getBasePath()
+
   return (
     <ul className="flex mx-auto my-8 w-2/3 justify-center">
       {hasPreviousPage && (
-        <li className="mx-8 text-xl">
+        <li className="mx-4 text-xl md:mx-8">
           <Link
-            to={currentPage - 1 === 1 ? `/blog` : `/blog/${currentPage - 1}`}
+            to={
+              currentPage - 1 === 1
+                ? `/${basePath}`
+                : `/${basePath}/${currentPage - 1}`
+            }
           >
             Prev
           </Link>
@@ -22,31 +60,29 @@ export function BlogListPageNumbers({
       {Array.from({
         length: 5,
       }).map((_, index) => {
-        let indexOffset = 2
-        let pageNumber
-
-        if (currentPage > indexOffset) {
-          pageNumber = currentPage + index - indexOffset
-        } else {
-          pageNumber = index + 1
-        }
+        const pageNumber = getPageNumber(index)
         if (pageNumber > pageCount) return null
 
         return (
           <li
-            className={`mx-8 text-xl ${
-              decodeURI(path).match(pageNumber) ? "font-bold" : ""
+            key={index}
+            className={`mx-4 text-xl md:mx-8 ${
+              pageNumberIsActive(pageNumber, decodeURI(path)) ? "font-bold" : ""
             }`}
           >
-            <Link to={pageNumber === 1 ? `/blog` : `/blog/${pageNumber}`}>
+            <Link
+              to={
+                pageNumber === 1 ? `/${basePath}` : `/${basePath}/${pageNumber}`
+              }
+            >
               {pageNumber}
             </Link>
           </li>
         )
       })}
       {hasNextPage && (
-        <li className="mx-8 text-xl">
-          <Link to={`/blog/${currentPage + 1}`}>Next</Link>
+        <li className="mx-4 text-xl md:mx-8">
+          <Link to={`/${basePath}/${currentPage + 1}`}>Next</Link>
         </li>
       )}
     </ul>
