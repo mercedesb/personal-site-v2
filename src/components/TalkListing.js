af27f6c6759e97f9cefade5e1bcac9843a775e26
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-08-11 19:32:57 -0500

    Dynamically select a complete class name

--- src/components/TalkListing.js
+++ src/components/TalkListing.js
@@ -24,17 +24,31 @@ export function TalkListing() {
     }
   `)
 
+  // Dynamically select a complete class name so PurgeCSS won't remove it
+  const getBorderColorClass = color => {
+    let borderColor = "border-brown-900"
+    if (color === "purple") {
+      borderColor = "border-purple-500"
+    } else if (color === "red") {
+      borderColor = "border-red-500"
+    } else if (color === "yellow") {
+      borderColor = "border-yellow-500"
+    } else if (color === "gray") {
+      borderColor = "border-gray-500"
+    }
+    return borderColor
+  }
+
   return (
     <div className="flex flex-wrap lg:-mr-48">
       {data.allContentfulTalkPage.edges.map(({ node }, index) => {
-        const className = `border-${node.color}-500`
         return (
           <TalkListItem
             title={node.title}
             urlSegment={node.urlSegment}
             iconSvg={node.iconSvg ? node.iconSvg.svg.svg : ""}
             iconCssClass={node.iconCssClass}
-            className={className}
+            className={getBorderColorClass(node.color)}
             index={index}
           />
         )
