e3511e5ad1d73007493d84c5fd2aa851a1715d4b
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-08-13 17:40:02 -0500

    add lang attr

--- src/components/HtmlHead.js
+++ src/components/HtmlHead.js
@@ -24,7 +24,11 @@ export function HtmlHead({ title, description, socialImage, path }) {
     : "Mercedes Bernard is a values-driven, growth-oriented software engineering leader"
 
   return (
-    <Helmet>
+    <Helmet
+      htmlAttributes={{
+        lang: "en-US",
+      }}
+    >
       <meta charSet="utf-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
