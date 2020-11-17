3cb57671812768a1d039520e784d2162f238bab4
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-11-17 16:44:38 -0600

    make it match Kate's

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,7 @@
 import "fontsource-libre-baskerville"
+import "fontsource-libre-baskerville/400.css"
 import "fontsource-libre-baskerville/700.css"
 import "fontsource-poppins"
+import "fontsource-poppins/400.css"
 import "fontsource-poppins/700.css"
 import "./src/styles/app.scss"
