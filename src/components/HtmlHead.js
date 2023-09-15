72ee5977c7dc0b60118db0236501d9a52da5a572
Mercedes Bernard <mercedesrbernard@gmail.com>
2023-09-15 13:29:09 -0500

    add language back to <html> for a11y

--- src/components/HtmlHead.js
+++ src/components/HtmlHead.js
@@ -30,6 +30,7 @@ export function HtmlHead({
 
   return (
     <>
+      <html lang="en-US" />
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
